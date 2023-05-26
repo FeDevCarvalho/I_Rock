c5177adcf146d05fc847ab0e0b80556251df983b
FeDevCarvalho <fecarvalho066@gmail.com>
2023-05-26 12:14:51 -0300

    add onload function

---
+++ docs/script.js
@@ -0,0 +1,7 @@
+function carregou() {
+    alert(`Pagina carregada com sucesso\nAgora você já pode aproveitar o seu Rock Pet Virtual`)
+};
+
+function clicou() {
+    alert(`Fia da puta, clicou em mim porque`)
+}
